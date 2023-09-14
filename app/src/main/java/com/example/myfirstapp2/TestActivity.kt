e6309fe8f6a180aef59fa8e285d0d447e01ea0e0
antoncoral <antoncoral2022@gmail.com>
2023-09-14 09:29:02 +0300

    тестовый комит 2

--- app/src/main/java/com/example/myfirstapp2/TestActivity.kt
+++ app/src/main/java/com/example/myfirstapp2/TestActivity.kt
@@ -1,17 +1,26 @@
 package com.example.myfirstapp2
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myfirstapp2.databinding.ActivityTestBinding
 
 class TestActivity : AppCompatActivity() {
     private lateinit var bindingClass: ActivityTestBinding
 
+    val lostArray = arrayOf(1000, 2300, 45000, 65000, 6500, 400)
+    val earnArray = arrayOf(15000, 300, 345000, 5000, 16500, 3400)
+    val resultArray = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val name = resources.getStringArray(R.array.names)
 
-        bindingClass = ActivityTestBinding.inflate(layoutInflater)
-        setContentView(bindingClass.root)
+        for((index, name) in name.withIndex()) {
+
+            resultArray.add("Имя: $name - прибыль = ${earnArray[index] - lostArray[index]}")
+            Log.d("MyLog", "Статистика -/- ${resultArray[index]}")
+        }
 
     }
 
